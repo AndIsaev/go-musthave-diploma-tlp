7367fbea0528a9675f14ad1369441cf05e93957e
AndIsaev <and.isaev.v@yandex.ru>
2024-10-20 16:56:52 +0300

    init server

---
+++ cmd/gophermart/configuration/config.go
@@ -0,0 +1,23 @@
+package configuration
+
+import (
+	"flag"
+	"os"
+)
+
+type Config struct {
+	Address string `env:"RUN_ADDRESS"`
+}
+
+func NewConfig() *Config {
+	cfg := &Config{}
+
+	flag.StringVar(&cfg.Address, "a", "localhost:8080", "address for your server")
+
+	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDRESS"); envRunAddr != "" {
+		cfg.Address = envRunAddr
+	}
+	return cfg
+}
