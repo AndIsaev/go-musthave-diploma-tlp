656e289eda21058df15ff2c82d2029f8205c5dfb
AndIsaev <and.isaev.v@yandex.ru>
2024-10-21 07:52:50 +0300

    added new variable for parsing

--- cmd/gophermart/configuration/config.go
+++ cmd/gophermart/configuration/config.go
@@ -7,17 +7,28 @@ import (
 
 type Config struct {
 	Address string `env:"RUN_ADDRESS"`
+	DB      string `env:"DATABASE_URI"`
+	Accural string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
 
 func NewConfig() *Config {
 	cfg := &Config{}
 
 	flag.StringVar(&cfg.Address, "a", "localhost:8000", "address for your server")
+	flag.StringVar(&cfg.DB, "d", "", "DATABASE_URI")
 
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("RUN_ADDRESS"); envRunAddr != "" {
 		cfg.Address = envRunAddr
+	}
+
+	if envDB := os.Getenv("DATABASE_URI"); envDB != "" {
+		cfg.DB = envDB
+	}
+
+	if envAccural := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); envAccural != "" {
+		cfg.DB = envAccural
 	}
 	return cfg
 }
